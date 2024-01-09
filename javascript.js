999696e90f3113de54de172f5b326ef0e20f31ef
Mizakson <maxwellizakson@gmail.com>
2024-01-09 10:25:17 -0500

    Refactor Gameboard, GameFlow IIFE -> function

--- javascript.js
+++ javascript.js
@@ -1,16 +1,24 @@
 // tic tac toe js
 
 
-const Gameboard = (function(){
-    const gameboard = [];   
-})();
+function Gameboard() {
+    const rows = 3;
+    const cols = rows;
+    const gameboard = [];
+}
 
 
-const Player = function(name, marker) {
-    return { name, marker }; 
-};
+const GameFlow = function(name1, name2) {
+    const gameboard = Gameboard();
 
-
-const GameFlow = (function(){
-
-})();
+    const players = [
+        {
+            name: name1,
+            marker: 'X'
+        },
+        {
+            name: name2,
+            marker: 'O'
+        }
+    ]
+}
