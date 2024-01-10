1da74432c3ebad937b46298094d76b17b30f0c1d
Mizakson <maxwellizakson@gmail.com>
2024-01-10 10:24:18 -0500

    Testing console game logic
    changePlayerTurn method

--- javascript.js
+++ javascript.js
@@ -1,114 +1,53 @@
 // tic tac toe js
 
-// Initial logic for this project used 
-// from the article 'Building a House From The Inside Out'
-// by Alex Younger
-
-// Looked at his logic behind a connect four console game example
-// and am working on refactoring that logic into this project
-
 function Gameboard() {
-    const rows = 3;
-    const cols = rows;
-    const gameboard = [];
+    let emptyRow = [[0], [0], [0]];
 
-    const innerRow = 0;
+    let emptyBoard = [
+        emptyRow,
+        emptyRow,
+        emptyRow
+    ];
 
-    // gameboard creation
-    // common way of making 2d array
-    for (let i = 0; i < rows; i++) {
-        // row 0 is the top row
-        gameboard[i] = [[], [], []];
-        for (let j = 0; j < cols; j++) {
-            // col j is left-most column
-            gameboard[j] = [[], [], []];
-        }
-    };
-
-    // ui render
-    const getGameboard = () => gameboard; 
-
-    const addMarker = () => {
-        // have to add to specific row, col 
-    }
-
-    const showGameBoard = () => {
-        // map is the same as using a loop, just a method for arrays
-        const filledBoard = gameboard.map((row) => row.map((cell) => cell.getVal()));
-        console.log(filledBoard);
-    }
-
-
-    return { getGameboard, addMarker, showGameBoard };
-
-}
-
-// each box on the board
-function Cell() {
-    // 3 possible vals
-    // 0 - empty
-    // 1 - player1 marker
-    // 2 - player2 marker
-
-    let val = 0;
-
-    const addMarker = (player) => {
-        val = player;
-    }
-
-    const getVal = () => val;
-
-    return { addMarker, getVal };
+    return { emptyBoard };
 };
 
-
-const GameFlow = function(name1, name2) {
-    const gameboard = Gameboard();
-
-    const players = [
+function gameController(player1, player2) {
+    
+    let players = [
         {
-            name: name1,
+            name: player1,
             marker: 'X'
         },
         {
-            name: name2,
+            name: player2,
             marker: 'O'
         }
     ];
 
-    // default turn is player 1
-    let activePlayer = players[0];
+    let board = Gameboard().emptyBoard;
 
-    const changePlayerTurn = () => {
-        // ternary operator
-        // if players[0] is the active player, then switch to players[1]
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
+    let activePlayer = players[0];
 
     const getActivePlayer = () => activePlayer;
 
-    const newRound = () => {
-        gameboard.showGameBoard();
-        console.log(`${getActivePlayer().name}'s turn...`);
+    function changePlayerTurn(player) {
+        if (player === activePlayer) {
+            player = players[1];
+            console.log(`${activePlayer}'s turn...`);
+        } else {
+            player = activePlayer;
+            console.log(`${players[1]}'s turn...`);
+        }
+    }
+
+    function addMarker(col, row, player) {
+        
+        player = getActivePlayer;
+        board[col][row] = [getActivePlayer().marker];
+        return board;
     };
 
-    const playRound = () => {
-        console.log(`Adding ${getActivePlayer().name}'s ${getActivePlayer().marker} into
-        column ${col}, row ${row}`);
-
-        gameboard.addMarker(col, row, getActivePlayer().marker);
-
-        // add game winner logic here
-        // add game win message here
-
-        changePlayerTurn();
-        newRound();
-    };
-    
-    newRound();
-
-    return { newRound, getActivePlayer };
+    return { players, changePlayerTurn, addMarker }; 
 
 }
-
-const game = GameFlow('Max', 'Bob');
