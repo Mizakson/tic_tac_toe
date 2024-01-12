79bf805fc065b3e73dde798eba8db5416db97623
Mizakson <maxwellizakson@gmail.com>
2024-01-12 10:26:22 -0500

    Add changeMarker method to gameController module
    create Cell module

--- javascript.js
+++ javascript.js
@@ -2,15 +2,37 @@
 
 function Gameboard() {
     let board = [];
-    let row = ['0', '0', '0'];
 
-    for (let i = 0; i < 3; i++) {
-        board.push(row);
-    };
+    // fill gameboard arr with 9 cells
+    // with 0 as value
+
+    let rows = 3;
+    let columns = 3;
+    
+    //two-dimensional array
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i][j] = Cell().value;
+      }
+    }
+
 
     const getGameboard = () => board;
 
+
     return { board, getGameboard };
+
+};
+
+function Cell() {
+    let value = 0;
+
+    function changeValue(player) {
+        value = player;
+    }
+
+    return { value };
 
 };
 
@@ -30,17 +52,24 @@ function gameController(player1, player2) {
         }
     ];
 
+
     let activePlayer = players[0];
+
+    function changeMarker(col, row, player) {
+        player = activePlayer.marker;
+        board[col][row] = player;
+    };
 
     // connect 4 console example switch turn method
     const switchTurn = () => {
+        console.log('switching turn...');
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        console.log(`${activePlayer}'s turn...`);
     }
 
     const getActivePlayer = () => activePlayer;
 
-    return { players }; 
+
+    return { players, switchTurn, getActivePlayer, changeMarker, board }; 
 
 }
-
-const test = Gameboard();
