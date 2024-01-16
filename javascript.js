7f370507c394fdd1f6b00899acbc142002c5cdd8
Mizakson <maxwellizakson@gmail.com>
2024-01-16 13:08:16 -0500

    Create initial Gameboard module

--- javascript.js
+++ javascript.js
@@ -1,10 +1,26 @@
 // tic tac toe js
 
+const gameBoard = (function() {
+    let gameboard = ["","","","","","","","",""];
+
+    const render = () => {
+        let gameboardHTML = "";
+        
+        gameboard.forEach((cell, index) => {
+            gameboardHTML += `<div class="cell" id="cell-${index}">${cell}</div>`;
+        })
+        document.querySelector("#gameboard").innerHTML = gameboardHTML;
+    }
+
+    return { render };
+
+})();
+
 const startBtn = document.querySelector("#start");
 const restartBtn = document.querySelector("#restart");
 
 startBtn.addEventListener("click", () => {
-    alert("TEST");
+    GameController.startMatch();
 });
 
 restartBtn.addEventListener("click", () => {
