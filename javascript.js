e72eba1470712326b4a9e86a447221bb276781b4
Mizakson <maxwellizakson@gmail.com>
2024-01-16 12:56:39 -0500

    Test button onclick method

--- javascript.js
+++ javascript.js
@@ -1,2 +1,12 @@
 // tic tac toe js
 
+const startBtn = document.querySelector("#start");
+const restartBtn = document.querySelector("#restart");
+
+startBtn.addEventListener("click", () => {
+    alert("TEST");
+});
+
+restartBtn.addEventListener("click", () => {
+    alert("TEST");
+});
